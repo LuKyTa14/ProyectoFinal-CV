6dad5a799eddcf48299c8289f849db600641efec
Lucas <fanchinlucas@gmail.com>
2023-01-24 04:50:12 -0300

    Actualizaciones variadas

--- JS/funciones.js
+++ JS/funciones.js
@@ -3,12 +3,13 @@
 //CAMBIAR TEXTO
 document.getElementById("elimtext").onclick = function () {
     document.getElementById("text").innerHTML = "Ahora no vas a parar de tocar botones";
+    document.getElementById("elimtext").style.display = "none";
 }
 
 
 //CAMBIAR COLORES 
 document.getElementById("colorfondo").onclick = function () {
-    document.body.style.backgroundColor = "red";
+    document.body.style.backgroundColor = "rgb(255, 25, 40)";
 }
 
 document.getElementById("colornormal").onclick = function () {
@@ -19,11 +20,13 @@ document.getElementById("colornormal").onclick = function () {
 document.getElementById("mejorboton").onclick = function () {
     document.getElementById("mejorboton").style.backgroundColor = "gold";
     document.getElementById("mejorboton").style.color = "black";
+    document.getElementById("text").innerHTML = "Los botones azules no sirven";
 }
 
 document.getElementById("botonnormal").onclick = function () {
-    document.getElementById("mejorboton").style.backgroundColor = " rgb(50, 55, 86)";
+    document.getElementById("mejorboton").style.backgroundColor = "rgb(0, 9, 65)";
     document.getElementById("mejorboton").style.color = "white";
+    document.getElementById("text").innerHTML = "Juguemos un rato con <br> la Pagina";
 }
 
 //QUITAR BOTONES
@@ -45,5 +48,6 @@ document.getElementById("volverbotones").onclick = function() {
     document.getElementById("botonnormal").style.display = "block";
     document.getElementById("borrar").style.display = "block";
     document.getElementById("volverbotones").style.display = "none";
+    document.getElementById("text").innerHTML = "Juguemos de vuelta";
 }
 
